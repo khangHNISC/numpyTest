0ff1bd88165688b997bd6d3d55b6be70b2166850
Khang Le <khangle@Khangs-Mac-mini.local>
2023-08-08 21:27:37 +0700

    add arithemetic

--- test_create_numpy.py
+++ test_create_numpy.py
@@ -9,8 +9,15 @@ class TestCreateNumpy(unittest.TestCase):
     @parameterized.expand([
         ("t1", np.zeros(2), dtype('float64'), 1, (2,), 2),
         ("t2", np.ones(3, dtype=np.int32), dtype('int32'), 1, (3,), 3),
+        ("t3", np.empty(2), dtype('float64'), 1, (2,), 2),  # random
+        ("t4", np.arange(4), dtype('int64'), 1, (4,), 4),
+        ("t5", np.arange(2, 9, 2), dtype('int64'), 1, (4,), 4),
+        ("t6", np.linspace(2, 8, num=4), dtype('float64'), 1, (4,), 4),
+
     ])
     def test_type(self, name, ar, dtype, ndim, shape, size):
+        print(name)
+        print(ar)
         self.assertEqual(ar.dtype, dtype, "number " + name)
         self.assertEqual(ar.ndim, ndim, "number " + name)
         self.assertEqual(ar.shape, shape, "number " + name)
---
+++ test_sort_concat.py
@@ -0,0 +1,22 @@
+import unittest
+
+import numpy as np
+from numpy import dtype
+from parameterized import parameterized
+
+
+class TestArithmetic(unittest.TestCase):
+    @parameterized.expand([
+        ("t1", np.array([1, 2, 3, 4]), np.array([5, 6, 7, 8]), np.arange(1, 9)),
+        ("t2", np.array([[1, 2], [3, 4]]), np.array([[5, 6]]), np.array([[1, 2], [3, 4], [5, 6]])),
+    ])
+    def test_add(self, name, ar1, ar2, e):
+        print(name)
+        self.assertTrue(np.array_equal(np.concatenate((ar1, ar2), axis=0), e))
+
+    @parameterized.expand([
+        ("t1", np.array([2, 1, 5, 3, 7, 4, 6, 8]), np.arange(1, 9))
+    ])
+    def test_sort(self, name, ar1, e):
+        print(name)
+        self.assertTrue(np.array_equal(np.sort(ar1), e))
